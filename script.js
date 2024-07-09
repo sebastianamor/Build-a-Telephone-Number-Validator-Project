e91df4f8244044ef487842d2845b26eeee4d46b5
¨Jane <smoreira11@hotmail.com>
2024-07-09 22:20:51 +0900

    finish only ccs ...

--- script.js
+++ script.js
@@ -3,35 +3,21 @@ const checkBtn = document.getElementById("check-btn")
 const clearBtn = document.getElementById("clear-btn")
 const result = document.getElementById("results-div")
 
+function checkNumber(){
+ const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
 
+if (inputuser.value == "") {
+ return alert("Please provide a phone number");} 
+  
+  let item = document.createElement("li")
+  result.appendChild(item);
 
-  const countryCode = '^(1\\s?)?';
-  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
-  const spacesDashes = '[\\s\\-]?';
-  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
-  const phoneRegex = new RegExp(
-    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
-  );
+  if (regex.test(inputuser.value)){
+    item.innerHTML = "Valid US number: "+inputuser.value;
+  } else {
+    item.innerHTML = "Invalid US number: "+inputuser.value;
+  }}
 
-const denyList = [countryCode,areaCode ,spacesDashes , phoneNumber];
+checkBtn.addEventListener("click",checkNumber);
 
-const isSpam = (msg) => denyList.some((phoneRegex) => phoneRegex.test(msg));
-
-
-  checkBtn.addEventListener('click', () => {
-    if (inputuser.value == "") {
-        alert("Please provide a phone number"); 
-        return; }
-        result.textContent = isSpam(inputuser.value)
-    ? "Invalid US number: 5555555"
-    : "Valid US number: 1 555-555-5555";
-  inputuser.value = "";
-});
-
-clearBtn.addEventListener('click', function() {
-    document.getElementById('results-div').innerHTML = ''});
-
-checkBtn.addEventListener('click', () => {
-    if (inputuser.value == "1 555-555-5555") {
-     document.getElementById('results-div').innerHTML = 'Valid US number: 1 555-555-5555'}  {;   
- }}); 
+clearBtn.addEventListener('click', () => {result.innerHTML = '';}) 
